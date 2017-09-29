26856d15474d8d0c2eccac358a7e28dfa8b4719c
Chandrakant <chandrakant7892@gmail.com>
2017-09-29 10:43:28 +0530

    Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |i|
+	FoodItem.create(name: "Food#{i}", price: 10*i)
+end
+
+10.times do |i|
+	Locality.create(locality_name: "Locality#{i}")
+end
+
+10.times do |i|
+	start_time = "0#{i}:00".to_time
+	end_time = "0#{i+1}:00".to_time
+	TimeSlot.create(start_time: start_time, end_time: end_time)
+end
