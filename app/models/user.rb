b36066b942d66fbffe4d31ec2531395d8299121c
Chandrakant <chandrakant7892@gmail.com>
2017-09-29 10:28:19 +0530

    Model level validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :addresses
   has_many :orders
+  validates :mobile, :full_name, presence: true
+  validates :mobile, length: { in: 10..10 }
 end
